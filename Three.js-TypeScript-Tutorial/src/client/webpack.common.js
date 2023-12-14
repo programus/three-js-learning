a4c31193c3ae75476d9eacb7e88922af7482ae26
programus <programus@gmail.com>
2023-12-14 22:20:56 +0900

    initial the repo

---
+++ Three.js-TypeScript-Tutorial/src/client/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: [
+    './src/client/client.ts',
+    './src/client/css/main.css',
+  ],
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js',
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../../dist/client'),
+  },
+}
